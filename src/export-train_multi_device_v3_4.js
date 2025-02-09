c6cd56f2ae6c6f1f724f4d79d6592889166ec08d
boldak <boldak.andrey@gmail.com>
2025-02-09 19:14:51 +0200

    add multi device

--- src/export-train_multi_device_v3_4.js
+++ src/export-train_multi_device_v3_4.js
@@ -1,7 +1,7 @@
 const exportDataset = require("./utils/export-dataset")
 
 const run = async () => {
-	await exportDataset(require("../datasets/validation_multi_device_v3_4"))		
+	await exportDataset(require("../datasets/train_multi_device_v3_4"))		
 }
 
 run()
